412d98d2b016157f89cec013423c48bbd1262530
PoritonEE <orihefetz@campus.technion.ac.il>
2025-08-11 15:39:46 +0300

    everything to fit the wavelet model
    Signed-off-by: PoritonEE <orihefetz@campus.technion.ac.il>

--- training/callbacks.py
+++ training/callbacks.py
@@ -1,9 +1,13 @@
-
 from pytorch_lightning.callbacks import LearningRateMonitor, EarlyStopping, ModelCheckpoint
 
 def default_callbacks():
     return [
         LearningRateMonitor(logging_interval='epoch'),
-        EarlyStopping(monitor='val/Accuracy', mode='max', patience=5),
-        ModelCheckpoint(monitor='val/Accuracy', mode='max', save_top_k=1, filename='{epoch}-{val_Accuracy:.3f}')
+        EarlyStopping(monitor='val/loss', mode='min', patience=5),
+        ModelCheckpoint(
+            monitor='val/loss',
+            mode='min',
+            save_top_k=1,
+            filename='{epoch}-{val_loss:.3f}'
+        ),
     ]
